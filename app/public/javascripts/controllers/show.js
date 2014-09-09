8bf1ca408af2155c9ac8706b32e308deb0aad088
axross <kou@axross.org>
2014-09-09 11:26:45 +0900

    Fix entry fetch logic

--- app/public/javascripts/controllers/show.js
+++ app/public/javascripts/controllers/show.js
@@ -4,28 +4,21 @@ module.exports = function(app) {
   app.controller('showController', ['$scope', '$stateParams', 'Entry', function($scope, $stateParams, Entry) {
     // Unscoped values
     var tabId     = $stateParams.id || null;
-    var nowOffset = 0;
+    var lastId    = 0;
     var isLoading = false;
     var isEnd     = false;
 
-    var fetchEntries = function() {
-      if (tabId) {
-        Entry.getByTabId(tabId, nowOffset, function(entries) {
-          $scope.entries = $scope.entries.concat(entries);
-          nowOffset += entries.length;
-          if (entries.length !== 20) { isEnd = true; }
+    var fetchEntry = function() {
+      Entry.get({
+        lastId: lastId
+      }, function(entries, _lastId, _isEnd) {
+        lastId = _lastId;
+        isEnd  = _isEnd;
 
-          setTimeout(function() { isLoading = false }, 500);
-        });
-      } else {
-        Entry.getAll(nowOffset, function(entries) {
-          $scope.entries = $scope.entries.concat(entries);
-          nowOffset += entries.length;
-          if (entries.length !== 20) { isEnd = true; }
+        $scope.entries = $scope.entries.concat(entries);
 
-          setTimeout(function() { isLoading = false }, 500);
-        });
-      }
+        setTimeout(function() { isLoading = false }, 500);
+      });
     };
 
     // Scoped values
@@ -41,13 +34,13 @@ module.exports = function(app) {
 
       if (!isLoading && windowHeight + scrollTop + 512 > contentHeight) {
         isLoading = true;
-        fetchEntries();
+        fetchEntry();
       }
     });
 
     // Initialize
     $scope.$on('$stateChangeSuccess', function() {
-      fetchEntries();
+      fetchEntry();
     });
   }]);
 };
--- app/public/javascripts/services/entry.js
+++ app/public/javascripts/services/entry.js
@@ -1,22 +1,32 @@
 module.exports = function(app) {
   app.factory('Entry', ['$http', function($http) {
-    var getAll = function(offset, callback) {
-      $http.get('/entry', { params: { offset: offset } })
+    var get = function(option, callback) {
+      if (!option) return callback(null);
+
+      var lastId = option.lastId || 2147483647;
+      var limit  = option.limit  || 20;
+
+      $http.get('/entry', { params: { last_id: lastId, limit: limit } })
+        .error(function(data, err) {
+          errorCallback(data, err, callback);
+        })
         .success(function(data, err) {
-          callback(data.entries);
+          var entries = data.entries;
+
+          // entires, lastId, isEnd
+          callback(entries, entries[entries.length - 1].id, (entries.length !== limit));
         });
+
     };
 
-    var getByTabId = function(tabId, offset, callback) {
-      $http.get('/entry/' + tabId, { params: { offset: offset } })
-        .success(function(data, err) {
-          callback(data.entries);
-        });
+    var errorCallback = function(data, err) {
+      console.error(err);
+
+      callback(null);
     };
 
     return {
-      getAll:     getAll,
-      getByTabId: getByTabId
+      get: get
     };
   }]);
 };
