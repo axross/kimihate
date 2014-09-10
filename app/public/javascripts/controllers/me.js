1dd95e9591c6356fd123d9f71e4f71d138b54976
axross <kou@axross.org>
2014-09-10 22:02:28 +0900

    Modify me service

--- app/public/javascripts/controllers/me.js
+++ app/public/javascripts/controllers/me.js
@@ -1,10 +1,11 @@
 module.exports = function(app) {
   app.controller('meController', ['$scope', '$stateParams', function($scope, $stateParams) {
     // Unscoped values
+    var journals;
 
 
     // Scoped values
-    $scope.tabs = [
+    $scope.journals = [
       { id: 1, title: 'Javascript', stars: 5, words: ['Javascript', 'Node.js', 'Angular'] },
       { id: 1, title: 'Javascript', stars: 5, words: ['Javascript', 'Node.js', 'Angular'] },
       { id: 1, title: 'Javascript', stars: 5, words: ['Javascript', 'Node.js', 'Angular'] }
